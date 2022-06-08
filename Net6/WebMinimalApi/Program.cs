d806c1b57504658ce90fd2e94fe2fa0cd85506f8
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-08 12:23:32 +0200

    Authentication & Authorization

--- Net6/WebMinimalApi/Program.cs
+++ Net6/WebMinimalApi/Program.cs
@@ -1,12 +1,43 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+var signingKey = Encoding.UTF8.GetBytes(Guid.NewGuid().ToString());
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(signingKey),
+            RequireExpirationTime = true
+        };
+    });
+builder.Services.AddAuthorization();
 
 // Add services to the container.
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 var summaries = new List<string>
 {
@@ -27,9 +58,34 @@ app.MapGet("/weatherforecast", () =>
 });
 
 
-app.MapGet("/summaries", () => summaries);
+app.MapGet("/summaries",  () => summaries).RequireAuthorization();
 app.MapPost("/summaries", (Summary value) => summaries.Add(value.Name));
-app.MapDelete("/summaries", (string value) => summaries.Remove(value));
+app.MapDelete("/summaries", [Authorize(Roles = "Admin")] (string value) => summaries.Remove(value));
+
+app.MapGet("/login", async context =>
+{
+    if (context.Request.Headers.TryGetValue("username", out var username) &&
+        context.Request.Headers.TryGetValue("password", out var password))
+    {
+        if (username != "admin" || password != "pass")
+            return;
+
+        var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor();
+
+        tokenDescriptor.Subject = new ClaimsIdentity(claims);
+        tokenDescriptor.Expires = DateTime.Now.AddSeconds(30);
+        tokenDescriptor.SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(signingKey), SecurityAlgorithms.HmacSha256);
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        await context.Response.WriteAsync(tokenHandler.WriteToken(token));
+    }
+});
 
 app.Run();
 
