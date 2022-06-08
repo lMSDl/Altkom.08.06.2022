c79d230cb418e04b4f4adfc44192cec8aa823f7d
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-08 11:22:50 +0200

    record

---
+++ Net6/ConsoleApp/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    //public record Person(string FirstName)
+    public record class Person(string FirstName)
+    {
+        //public Person(string firstName)
+        //{
+        //    FirstName = firstName;
+        //}
+        //public string? FirstName { get; set; }
+        public string? LastName { get; init; }
+
+        public Class? SomeClass { get; set; }
+    }
+
+    public record struct Address 
+    {
+        public Address(string street)
+        {
+            Street = street;
+            PostCode = null;
+        }
+
+        public string? Street { get; set; }
+        public string? PostCode { get; init; }
+    }
+}
--- Net6/ConsoleApp/Program.cs
+++ Net6/ConsoleApp/Program.cs
@@ -34,6 +34,27 @@ Console.WriteLine(myClass.Name);
 
 
 
+var person1 = new Person("Ewa") { LastName = "Ewowska" };
+//person1.FirstName = "Ewa";
+//person1.LastName = "Ewowska";
+person1.SomeClass = new Class();
+
+//var person2 = new Person("Ewa") { LastName = "Ewowska" };
+//person2.FirstName = "Ewa";
+//person2.LastName = "Ewowska";
+var person2 = person1 with { }; 
+//person2.SomeClass = new Class();
+
+//var person3 = new Person("Monika") { LastName = "Ewowska" };
+var person3 = person1 with { FirstName = "Monika" };
+//person3.FirstName = "Monika";
+//person3.LastName = "Ewowska";
+
+Console.WriteLine($"{person1} vs {person2} = {person1 == person2}");
+Console.WriteLine($"reference {person1} vs {person2} = {ReferenceEquals(person1, person2)}");
+Console.WriteLine($"{person1} vs {person3} = {person1 == person3}");
+
+
 void ShowText()
 {
     JsonConvert.SerializeObject(new Class());
