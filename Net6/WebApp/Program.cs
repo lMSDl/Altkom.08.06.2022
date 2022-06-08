495397c1c17d02d59ed2a2f261623b93d47bfd6c
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-08 11:35:25 +0200

    Routing

---
+++ Net6/WebApp/Program.cs
@@ -0,0 +1,39 @@
+using WebApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<RandomNumberService>();
+
+var app = builder.Build();
+
+
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine(context.GetEndpoint()?.DisplayName ?? "null");
+
+    await next(context);
+
+});
+
+app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine(context.GetEndpoint()?.DisplayName ?? "null");
+
+    await next(context);
+
+});
+
+app.MapGet("/", () => "Hello World!");
+
+
+//app.UseEndpoints(x =>
+//{
+//    x.MapGet("/", () => "Hello World!");
+
+//});
+
+
+app.Run();
---
+++ Net6/WebApp/Services/RandomNumberService.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApp.Services
+{
+    public class RandomNumberService
+    {
+        private Random Random { get; }
+
+        public RandomNumberService()
+        {
+            Random = new Random();
+        }
+
+        public int GetInt()
+        {
+            return Random.Next();
+        }
+    }
+}
